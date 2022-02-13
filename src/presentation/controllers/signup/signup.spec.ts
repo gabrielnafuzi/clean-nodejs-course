31c43f5d688a8077102ba521836221b44277cebe
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-02-13 02:55:19 -0300

    chore: add padding-line-between-statements eslint rule

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -40,6 +40,7 @@ const makeSut = (): SutTypes => {
 describe('SignUp Controller', () => {
   it('should return 400 if no name is provided', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -56,6 +57,7 @@ describe('SignUp Controller', () => {
 
   it('should return 400 if no email is provided', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -72,6 +74,7 @@ describe('SignUp Controller', () => {
 
   it('should return 400 if no password is provided', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -88,6 +91,7 @@ describe('SignUp Controller', () => {
 
   it('should return 400 if no passwordConfirmation is provided', () => {
     const { sut } = makeSut()
+
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -99,6 +103,7 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+
     expect(httpResponse.body).toEqual(
       new MissingParamError('passwordConfirmation')
     )
