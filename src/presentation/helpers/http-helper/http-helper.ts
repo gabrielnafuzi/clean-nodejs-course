c2b7b9a7b634f73b8b79fc799e6e83d2b0f60898
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-02-14 03:16:46 -0300

    refactor: add generic types to http helpers

--- src/presentation/helpers/http-helper/http-helper.ts
+++ src/presentation/helpers/http-helper/http-helper.ts
@@ -1,14 +1,14 @@
 import { ServerError } from '../../errors'
 import { HttpResponse } from './../../protocols'
 
-export const badRequest = (error: Error): HttpResponse => {
+export const badRequest = (error: Error): HttpResponse<Error> => {
   return {
     statusCode: 400,
     body: error,
   }
 }
 
-export const serverError = (): HttpResponse => {
+export const serverError = (): HttpResponse<Error> => {
   return {
     statusCode: 500,
     body: new ServerError(),
