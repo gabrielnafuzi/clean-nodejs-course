bdb5ee3fe8c849d7be2a2cbbf886e68d8b392caf
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-02-16 09:16:37 -0300

    refactor: change test cases using 'test' to use 'it'

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -17,7 +17,7 @@ const makeSut = (): BcryptAdapter => {
 }
 
 describe('Bcrypt Adapter', () => {
-  test('should call bcrypt with correct values', async () => {
+  it('should call bcrypt with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
 
@@ -26,7 +26,7 @@ describe('Bcrypt Adapter', () => {
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('should return a hash on success', async () => {
+  it('should return a hash on success', async () => {
     const sut = makeSut()
 
     const hash = await sut.encrypt('any_value')
