7fcba4deb146e05a06f4083e5413b3de743be087
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-02-17 00:01:08 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-ts-api',
+  port: process.env.PORT || 5050,
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,18 @@
-import { app } from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import { env } from './config/env'
 
-const PORT = 5050
+const init = async () => {
+  try {
+    await MongoHelper.connect(env.mongoUrl)
 
-app.listen(PORT, () =>
-  console.log(`Server is listening on port http://localhost:${PORT}`)
-)
+    const { app } = await import('./config/app')
+
+    app.listen(env.port, () =>
+      console.log(`Server is listening on port http://localhost:${env.port}`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+init()
