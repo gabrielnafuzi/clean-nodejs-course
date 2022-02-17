07320007b594b522a8fb29b743c908150f6125f1
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-02-16 22:54:31 -0300

    feat: ensure cors is enabled

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 
 import { bodyParser } from '../middleware/body-parser/body-parser'
+import { cors } from '../middleware/cors/cors'
 
 export const setupMiddleware = (app: Express) => {
   app.use(bodyParser)
+  app.use(cors)
 }
---
+++ src/main/middleware/cors/cors.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction) => {
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-methods', '*')
+  res.set('access-control-allow-headers', '*')
+
+  next()
+}
