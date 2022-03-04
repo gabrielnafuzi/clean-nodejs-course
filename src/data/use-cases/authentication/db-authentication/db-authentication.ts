002ab900661a78265d44383c290112e549196de9
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-04 00:47:04 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/use-cases/authentication/db-authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication/db-authentication.ts
@@ -18,11 +18,21 @@ export class DbAuthentication implements Authentication {
       authentication.email
     )
 
-    if (account) {
-      await this.hashComparer.compare(authentication.password, account.password)
-      await this.tokenGenerator.generate(account.id)
+    if (!account) {
+      return null
     }
 
-    return null
+    const isValid = await this.hashComparer.compare(
+      authentication.password,
+      account.password
+    )
+
+    if (!isValid) {
+      return null
+    }
+
+    const accessToken = await this.tokenGenerator.generate(account.id)
+
+    return accessToken
   }
 }
