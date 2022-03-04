f8908c40d979d1a22a4f3fb7e8a18acc65335d4f
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-03-04 00:53:40 -0300

    test: ensure DbAuthentication throws if UpdateAccessTokenRepository throws

--- src/data/use-cases/authentication/db-authentication/db-authentication.spec.ts
+++ src/data/use-cases/authentication/db-authentication/db-authentication.spec.ts
@@ -207,4 +207,18 @@ describe('DbAuthentication UseCase', () => {
 
     expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token')
   })
+
+  it('should throw if UpdateAccessTokenRepository throws', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(updateAccessTokenRepositoryStub, 'update')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+
+    const promise = sut.auth(makeFakeAuthentication())
+
+    expect(promise).rejects.toThrow()
+  })
 })
